b5ad866929475cedc9eb2380563efd8cdcf8eaf1
Vinicius Katata Biondo <vkatatab@gmail.com>
2025-07-01 19:20:20 -0300

    testing new config

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: { 'api/index': 'src/vercel.ts' },
-    outDir: './dist',
-    target: 'node20',
-    format: ['cjs'],
-    bundle: true,
-    splitting: false,
-    dts: false,
-    sourcemap: false,
-    clean: true,
-    noExternal: ['@test-pod/database'],
-  },
-])
+export default defineConfig({
+  entry: { 'api/index': 'src/vercel.ts' },
+  outDir: './',
+  target: 'node20',
+  format: ['cjs'],
+  bundle: true,
+  splitting: false,
+  dts: false,
+  sourcemap: false,
+  clean: false,
+  minify: false,
+  noExternal: ['@test-pod/database'],
+  platform: 'node',
+})
