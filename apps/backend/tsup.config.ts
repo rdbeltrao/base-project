dd0d93de1864e2f9a638bac8f4d336033c61dc2d
Vinicius Katata Biondo <vkatatab@gmail.com>
2025-07-01 20:46:58 -0300

    removed unused

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     'api/index': './src/vercel.ts',
-    'api/app': './src/app.ts',
   },
   outDir: './',
   target: 'node20',
